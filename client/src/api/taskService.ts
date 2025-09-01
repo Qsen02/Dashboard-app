1be9fb0b4f1b6a268a37bd5eac1f8122738c1038
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-01 15:03:56 +0300

    Implemented task service

---
+++ client/src/api/taskService.ts
@@ -0,0 +1,28 @@
+import { Task } from "../types/task";
+import { del, get, post, put } from "./requester";
+
+const endpoint = "/tasks";
+
+export async function getTaskById(taskId: string) {
+	const task = await get(`${endpoint}/${taskId}`);
+	return task as Task;
+}
+
+export async function applyToTask(taskId: string) {
+	const updatedTask = await post(`${endpoint}/${taskId}/apply`, {});
+	return updatedTask as Task;
+}
+
+export async function changeTaskStatus(taskId: string, data: object) {
+	const updatedTask = await put(`${endpoint}/${taskId}/change-status`, data);
+	return updatedTask as Task;
+}
+
+export async function editTask(taskId: string, data: object) {
+	const updatedTask = await put(`${endpoint}/${taskId}/edit`, data);
+	return updatedTask as Task;
+}
+
+export async function deleteTask(taskId: string, projectId: string) {
+	await del(`${endpoint}/${taskId}/delete/in/${projectId}`);
+}
