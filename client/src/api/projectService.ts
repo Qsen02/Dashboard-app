92bfbd898a0d292bb5c04c4b691cff199e7b9293
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-01 14:55:13 +0300

    Implemented project service

---
+++ client/src/api/projectService.ts
@@ -0,0 +1,42 @@
+import { Project } from "../types/project";
+import { del, get, post, put } from "./requester";
+
+const endpoint = "/projects";
+
+export async function getProjectById(projectId: string) {
+	const project = await get(`${endpoint}/${projectId}`);
+	return project as Project;
+}
+
+export async function createProject(data: object) {
+	const project = await post(`${endpoint}`, data);
+	return project as Project;
+}
+
+export async function addMemberToProject(userId: string, projectId: string) {
+	const updatedProject = await put(
+		`${endpoint}/add-member/${userId}/in/${projectId}`,
+		{}
+	);
+	return updatedProject as Project;
+}
+
+export async function addTaskToProject(projectId: string, data: object) {
+	const updatedProject = await put(
+		`${endpoint}/add-task/in/${projectId}`,
+		data
+	);
+	return updatedProject as Project;
+}
+
+export async function editProjectName(projectId: string, data: object) {
+	const updatedProject = await put(
+		`${endpoint}/project-name/${projectId}/edit`,
+		data
+	);
+	return updatedProject as Project;
+}
+
+export async function deleteProject(projectId:string){
+    await del(`${endpoint}/${projectId}/delete`);
+}
