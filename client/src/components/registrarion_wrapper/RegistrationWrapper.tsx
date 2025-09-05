468bcea01adea8dbe9e9c2b0d9a67822253ca70b
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-05 12:10:55 +0300

    Implemented styling on registration wrapper

--- client/src/components/registrarion_wrapper/RegistrationWrapper.tsx
+++ client/src/components/registrarion_wrapper/RegistrationWrapper.tsx
@@ -3,6 +3,7 @@ import { RootState } from "../../redux/state/store";
 import { useState } from "react";
 import Login from "./login/Login";
 import Register from "./register/Register";
+import styles from "./registrationWrapperStyles.module.css";
 
 export default function RegistrationWrapper() {
 	const { theme } = useSelector((state: RootState) => state.theme);
@@ -18,15 +19,25 @@ export default function RegistrationWrapper() {
 
 	return (
 		<section
-			className={
-				theme === "light" ? "lightThemeNormal" : "darkThemeNormal"
-			}
+			className={`
+				${theme === "light" ? "lightThemeNormal" : "darkThemeNormal"} ${styles.wrapper}
+            `}
 		>
-				<div>
-					<button onClick={swapLogin}>Login</button>
-					<button onClick={swapRegister}>Register</button>
-				</div>
-				<div>{form === "login" ? <Login /> : <Register />}</div>
+			<div className={styles.buttonSection}>
+				<button
+					onClick={swapLogin}
+					className={form === "login" ? styles.selected : styles.notSelected}
+				>
+					Login
+				</button>
+				<button
+					onClick={swapRegister}
+					className={form === "register" ? styles.selected : styles.notSelected}
+				>
+					Register
+				</button>
+			</div>
+			<div>{form === "login" ? <Login /> : <Register />}</div>
 		</section>
 	);
 }
