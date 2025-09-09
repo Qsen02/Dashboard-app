7acdb48035ca8a76a5f15c1f49fc1a7db36fd93b
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-09 11:17:45 +0300

    Implemented loading spinner and server error message

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -10,32 +10,51 @@ export default function Home() {
 	const { projects, loading, error } = useGetUserProjects([], user?._id);
 
 	return (
-		<section className={styles.container}>
-			<div
-				className={`${styles.plusContainer} ${
-					theme === "light" ? "lightThemeSmoked" : "darkThemeLighter"
-				}`}
-				title="Create new project"
-			>
-				<i className="fa-solid fa-plus"></i>
-			</div>
-			<h2>The projects you are involved in</h2>
-			<section className={styles.projectWrapper}>
-				{projects.length > 0 ? (
-					projects.map((el) => (
-						<ProjectItem
-							key={el._id}
-							id={el._id}
-							name={el.name}
-							theme={theme}
-							membersCount={el.members.length}
-							tasksCount={el.tasks.length}
-						/>
-					))
-				) : (
-					<h2>No projects yet</h2>
-				)}
-			</section>
-		</section>
+		<>
+			{loading && !error ? (
+				<span className="loader"></span>
+			) : error ? (
+				<div
+					className={`
+						${theme === "light"
+							? "lightThemeNormal"
+							: "darkThemeNormal"}
+						errorMessage
+					`}
+				>
+					<h2>Server is not responding! Please try again later.</h2>
+				</div>
+			) : (
+				<section className={styles.container}>
+					<div
+						className={`${styles.plusContainer} ${
+							theme === "light"
+								? "lightThemeSmoked"
+								: "darkThemeLighter"
+						}`}
+						title="Create new project"
+					>
+						<i className="fa-solid fa-plus"></i>
+					</div>
+					<h2>The projects you are involved in</h2>
+					<section className={styles.projectWrapper}>
+						{projects.length > 0 ? (
+							projects.map((el) => (
+								<ProjectItem
+									key={el._id}
+									id={el._id}
+									name={el.name}
+									theme={theme}
+									membersCount={el.members.length}
+									tasksCount={el.tasks.length}
+								/>
+							))
+						) : (
+							<h2>No projects yet</h2>
+						)}
+					</section>
+				</section>
+			)}
+		</>
 	);
 }
