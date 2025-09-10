75ccdcabf640acc31a4d057ce970c262a7ead9d6
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-10 23:10:08 +0300

    Implemented project details structure

--- client/src/components/project_details/ProjectDetails.tsx
+++ client/src/components/project_details/ProjectDetails.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { RootState } from "../../redux/state/store";
 import { useGetOneProject } from "../../hooks/useProjects";
 
@@ -23,9 +23,64 @@ export default function ProjectDetails() {
 					<h2>Server is not responding! Please try again later.</h2>
 				</div>
 			) : (
-				<div>
-					<p>Project id: {project?._id}</p>
-				</div>
+				<section>
+					<h2>{project?.name}</h2>
+					<div>
+						<Link to={`/projects/${projectId}/add-member`}>
+							<button>Add member</button>
+						</Link>
+						<Link to={`/projects/${projectId}/members`}>
+							<button>Members</button>
+						</Link>
+						<Link to={`/projects/${projectId}/add-task`}>
+							<button>Add task</button>
+						</Link>
+					</div>
+					<section
+						className={
+							theme === "light"
+								? "lightThemeNormal"
+								: "darkThemeNormal"
+						}
+					>
+						<article>
+							<div>
+								<p>Pending</p>
+							</div>
+							<div>
+								{project?.tasks
+									.filter((el) => el.status === "pending")
+									.map((el) => (
+										<p key={el._id}>{el.title}</p>
+									))}
+							</div>
+						</article>
+						<article>
+							<div>
+								<p>In progress</p>
+							</div>
+							<div>
+								{project?.tasks
+									.filter((el) => el.status === "in-progress")
+									.map((el) => (
+										<p key={el._id}>{el.title}</p>
+									))}
+							</div>
+						</article>
+						<article>
+							<div>
+								<p>Completed</p>
+							</div>
+							<div>
+								{project?.tasks
+									.filter((el) => el.status === "completed")
+									.map((el) => (
+										<p key={el._id}>{el.title}</p>
+									))}
+							</div>
+						</article>
+					</section>
+				</section>
 			)}
 		</>
 	);
