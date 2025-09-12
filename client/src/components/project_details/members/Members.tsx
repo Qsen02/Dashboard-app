4fccbb0371a1fea9ea0ab9c80ff6fd9f29b5d35f
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-12 12:06:57 +0300

    Project members structure implemented

--- client/src/components/project_details/members/Members.tsx
+++ client/src/components/project_details/members/Members.tsx
@@ -1,10 +1,17 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { RootState } from "../../../redux/state/store";
+import { useGetProjectMembers } from "../../../hooks/useProjects";
 
 export default function Members() {
 	const { projectId } = useParams();
 	const { theme } = useSelector((state: RootState) => state.theme);
+	const { members, loading, error } = useGetProjectMembers([], projectId);
+
+	function onBack() {
+		history.back();
+	}
+
 	return (
 		<div className="modal">
 			<section
@@ -12,7 +19,19 @@ export default function Members() {
 					theme === "light" ? "lightThemeNormal" : "darkThemeNormal"
 				}
 			>
-				<h2>Works!</h2>
+				<button onClick={onBack}>X</button>
+				<h2>List of members</h2>
+				<section>
+					{loading && !error ? (
+						<span className="loader"></span>
+					) : error ? (
+						<p>Server is not responding, please try again later!</p>
+					) : members.length === 0 ? (
+						<p>No members yet.</p>
+					) : (
+						members.map((el) => <p key={el._id}>{el.username}</p>)
+					)}
+				</section>
 			</section>
 		</div>
 	);
