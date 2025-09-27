b031f43e98d87c5fba84a273ea033b0708155d45
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-27 10:46:15 +0300

    Task and scrollbar styling update

--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -7,7 +7,7 @@ import { SearchesModel } from "../models/searches";
 const localDB = "mongodb://127.0.0.1:27017/Dashboard";
 
 async function runDB() {
-	await mongoose.connect(process.env.PRODUCTION_DB || localDB);
+	await mongoose.connect(localDB);
     await TaskModel.init();
     await UserModel.init();
     await ProjectModel.init();
