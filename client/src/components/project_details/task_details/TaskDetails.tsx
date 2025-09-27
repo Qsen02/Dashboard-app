d3153179e6bcd44e1896f8e70db6f794d6f17073
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-09-27 12:29:45 +0300

    Implemented task details styling and added animation everywhere

--- client/src/components/project_details/task_details/TaskDetails.tsx
+++ client/src/components/project_details/task_details/TaskDetails.tsx
@@ -1,8 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useGetOneTask } from "../../../hooks/useTasks";
 import { transformDate } from "../../../utils/transformDate";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux/state/store";
+import styles from "./TaskDetailsStyles.module.css";
 
 export default function TaskDetails() {
+	const { user } = useSelector((state: RootState) => state.user);
+	const { theme } = useSelector((state: RootState) => state.theme);
 	const { taskId, projectId } = useParams();
 	const { task, loading, error } = useGetOneTask(null, taskId);
 
@@ -12,7 +17,11 @@ export default function TaskDetails() {
 
 	return (
 		<div className="modal">
-			<section>
+			<section
+				className={`${styles.wrapper} ${
+					theme === "light" ? "lightThemeNormal" : "darkThemeNormal"
+				}`}
+			>
 				{loading && !error ? (
 					<span className="loader"></span>
 				) : error ? (
@@ -32,6 +41,14 @@ export default function TaskDetails() {
 								: "No users applied yet"}
 						</p>
 						<p>Created on: {transformDate(task?.created_at)}</p>
+						{task?.ownerId._id === user?._id ? (
+							<div className={styles.buttonWrapper}>
+								<button>Edit</button>
+								<button>Delete</button>
+							</div>
+						) : (
+							""
+						)}
 					</>
 				)}
 			</section>
